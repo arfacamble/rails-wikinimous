1feffe1f5ba5485a767343a0c0d825d81dcc16e5
Arthur Campbell <arfa.camble@gmail.com>
2021-02-16 15:36:59 +0000

    article model and db seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  Article.create(
+    title: Faker::Movies::HitchhikersGuideToTheGalaxy.marvin_quote,
+    content: Faker::Movies::HitchhikersGuideToTheGalaxy.quote
+  )
+end
